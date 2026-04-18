63774bfe237f9a5fdcbc757bcfc3f8b4817c00af
drumdidam <drumdidam@gmail.com>
2026-04-18 17:10:43 +0200

    Domainjoin

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -924,23 +924,15 @@ require('lazy').setup({
       }
     end,
   },
-  { -- You can easily change to a different colorscheme.
-    -- Change the name of the colorscheme plugin below, and then
-    -- change the command in the config to whatever the name of that colorscheme is.
-    --
-    -- If you want to see what colorschemes are already installed, you can use `:Telescope colorscheme`.
-    --
-    --
-    'folke/tokyonight.nvim',
-    priority = 1000, -- Make sure to load this before all the other start plugins.
+  {
+    'bluz71/vim-moonfly-colors',
+    name = 'moonfly',
+    priority = 1000,
     config = function()
-      ---@diagnostic disable-next-line: missing-fields
-      require('tokyonight').setup {
-        styles = {
-          comments = { italic = false }, -- Disable italics in comments
-        },
-      }
-      vim.cmd.colorscheme 'tokyonight'
+      vim.g.moonflyTransparent = true
+      vim.cmd.colorscheme 'moonfly'
+      vim.api.nvim_set_hl(0, 'CursorLine', { bg = 'NONE' })
+      vim.api.nvim_set_hl(0, 'CursorLineNr', { bg = 'NONE' })
     end,
   },
 
