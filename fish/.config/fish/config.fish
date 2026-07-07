9a87d277c4ff68fa04e28b968f4459c5bbcd3182
drumdidam <drumdidam@gmail.com>
2026-07-07 22:32:29 +0200

    waybar update

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -5,4 +5,9 @@ if status is-interactive
     if type -q starship
         starship init fish | source
     end
+
+    # ls-Farben: other-writable (777) Ordner wie normale Verzeichnisse
+    # anzeigen (bold blue) statt mit grünem Hintergrund. Nicht genannte
+    # Typen behalten die eingebauten Default-Farben von GNU ls.
+    set -gx LS_COLORS "ow=01;34:tw=01;34"
 end
